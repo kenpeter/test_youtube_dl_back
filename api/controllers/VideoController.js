456401482c67968a6ff7d2f81161bbf95ae2f06a
kenpeter <figo2476@hotmail.com>
2017-02-25 22:26:05 +1100

    able to push video

--- api/controllers/VideoController.js
+++ api/controllers/VideoController.js
@@ -273,9 +273,51 @@ module.exports = {
       8000
     );
     
+  }
+  
+  
+  push_video: function (req, res) {
+  
+    // kill adb
+    exec("adb kill-server", Utils.puts);
+
+    // start adb
+    exec("adb start-server", Utils.puts);
+
+    //
+    console.log("\nwait for 8s\n");
     
+    var audio_path = __dirname + "/../../video";
+    
+    // https://stackoverflow.com/questions/32604656/what-is-the-glob-character
+    setTimeout(
+      function() {
+        glob(audio_path + "/**/*.mp4", function (er, files) {
+
+          files.map(function(singleFile) {
+            var arr = singleFile.split("/");
+            var lastElement = arr[arr.length - 1];
+            var tmpFileName = lastElement.replace(/[&\/\\#,+()$~%'":*?<>{}\ ]/g, "_");
+          
+            //https://stackoverflow.com/questions/22504566/renaming-files-using-node-js
+            var tmpFullFile = audio_path + "/"+ tmpFileName;
+            fs.rename(singleFile, tmpFullFile, function(err) {
+              if ( err ) console.log('ERROR: ' + err);
+              
+              var cmd = "adb push" + " " + tmpFullFile + " " + "/sdcard/Movies";
+              console.log(cmd);
+              exec(cmd, Utils.puts);
+            });
+            
+          }); // end files.map
+          
+        }); // end glob
+      }, 
+      8000
+    );
     
     
   }
+  
 };
 
